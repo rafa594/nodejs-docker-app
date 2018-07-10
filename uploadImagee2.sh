2d541a4f0f332a5f3e9729200a1865a54bc43e83
rafa594 <rafa5941@gmail.com>
2018-07-10 12:46:00 -0600

    try build 3

--- uploadImagee2.sh
+++ uploadImagee2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 eval $(aws ecr get-login --no-include-email --region us-east-2 | sed 's|https://||')
-docker build -t p5imagertut .
-docker tag p5imagertut 797409686075.dkr.ecr.us-east-2.amazonaws.com/p5rtut
+sudo docker build -t p5imagertut .
+sudo docker tag p5imagertut 797409686075.dkr.ecr.us-east-2.amazonaws.com/p5rtut
 #797409686075.dkr.ecr.us-east-2.amazonaws.com/p5rtut
-docker push 797409686075.dkr.ecr.us-east-2.amazonaws.com/p5rtut
+sudo docker push 797409686075.dkr.ecr.us-east-2.amazonaws.com/p5rtut
